57f674a26a9fceaad38b137bd615f12fbf556e2e
namecooo <kazuki.nakamura.develop@gmail.com>
2024-04-06 17:14:52 +0900

    GET / POST 処理

---
+++ src/todo/create-task.dto.ts
@@ -0,0 +1,4 @@
+// `POST` `/todo` が受け取るリクエストの定義
+export class CreateTaskDto {
+  title: string;
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('todo') // コントローラークラスの宣言とパスの宣言
 export class TodoController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('list') // HTTPメソッドとパスの指定
-  getList() {
-    return [
-      {
-        title: '牛乳を買いに行く',
-        due_on: '2022-12-24',
-        done: false,
-      },
-    ];
+  async getList() {
+    const result = await this.prisma.task.findMany();
+
+    return [...result];
+  }
+
+  @Post('')
+  // ルートに送信されたリクエスト情報を @Body デコレータで受け取ることが可能
+  // create-task.dto.ts で定義した方を使用することで、処理内でも型の情報が利用できる
+  async add(@Body() task: CreateTaskDto) {
+    // create で insert 処理
+    // createMany: 複数行の格納
+    // upsert: update or insert
+    await this.prisma.task.create({
+      data: task,
+    });
+
+    return {
+      status: 'OK',
+    };
   }
 }
